f220d11e3492d372ad4bd8f5755a2305a6e0a65d
unknown <Aviral.Kumar@edu.sait.ca>
2025-07-28 13:54:00 -0600

    Assignment almost complete

--- components/Footer.js
+++ components/Footer.js
@@ -10,8 +10,12 @@ export default function Footer() {
   padding: "1rem",
 }}
     >    
-      <p>© 2025 Internet Movies Rental Company. All Rights Reserved.</p>
-      <p>Contact: info@imr.com | +1 (403) 123-4567</p>
+      <p>
+        © 2025 Internet Movies Rental Company. All Rights Reserved.
+      </p>
+      <p>
+        Contact: info@imr.com | +1 (403) 123-4567
+      </p>
     </footer>
   );
 }
