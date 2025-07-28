c551fafa974bb0d92a1743593b428d72b7255ac5
PranavPatel2412 <pranavpatel5002@gmail.com>
2025-07-28 11:28:55 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
         marginTop: "2rem",
         fontSize: "0.9rem",
       }}
-    >    // hi 
+    >    
       <p>© 2025 Internet Movies Rental Company. All Rights Reserved.</p>
       <p>Contact: info@imr.com | +1 (403) 123-4567</p>
     </footer>
